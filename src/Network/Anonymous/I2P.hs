05f840af964fb6620483f246d77e4a42f7043ce7
Leon Mergen <leon@solatis.com>
2015-02-13 10:41:24 +0700

    Lays down initial file layout.

--- src/Network/Anonymous/I2P.hs
+++ src/Network/Anonymous/I2P.hs
@@ -1,2 +1,13 @@
 -- | Main interface to I2P
 module Network.Anonymous.I2P where
+
+import qualified Network.Anonymous.I2P.Internal as I
+
+-- | An I2P socket
+newtype Socket a = Socket {
+  socketType :: I.SocketType
+  }
+
+-- | Creates a new socket
+create :: I.SocketType -> Socket a
+create = Socket
---
+++ src/Network/Anonymous/I2P/Internal/Protocol.hs
@@ -0,0 +1,4 @@
+-- | Protocol description
+--
+-- Defines functions that handle the advancing of the SAM protocol.
+module Network.Anonymous.I2P.Internal.Protocol where
