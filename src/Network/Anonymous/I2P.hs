3821bba2a6a76a3bea366c85fe3c39f097a81315
Leon Mergen <leon@solatis.com>
2015-02-13 10:48:38 +0700

    Types was put in the wrong directory

--- src/Network/Anonymous/I2P.hs
+++ src/Network/Anonymous/I2P.hs
@@ -1,13 +1,13 @@
 -- | Main interface to I2P
 module Network.Anonymous.I2P where
 
-import qualified Network.Anonymous.I2P.Internal as I
+import qualified Network.Anonymous.I2P.Types as T
 
 -- | An I2P socket
 newtype Socket a = Socket {
-  socketType :: I.SocketType
+  socketType :: T.SocketType
   }
 
 -- | Creates a new socket
-create :: I.SocketType -> Socket a
+create :: T.SocketType -> Socket a
 create = Socket
