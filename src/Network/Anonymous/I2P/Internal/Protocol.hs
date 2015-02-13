862ed86e872ee6972961d6a58ccdfa62c1e8d7f1
Leon Mergen <leon@solatis.com>
2015-02-13 10:43:58 +0700

    Refactors types into own module

--- src/Network/Anonymous/I2P/Internal/Protocol.hs
+++ src/Network/Anonymous/I2P/Internal/Protocol.hs
@@ -1,4 +1,4 @@
 -- | Protocol description
 --
--- Defines functions that handle the advancing of the SAM protocol.
+-- Defines functions that handle the advancing of the SAMv3 protocol.
 module Network.Anonymous.I2P.Internal.Protocol where
---
+++ src/Network/Anonymous/Types.hs
@@ -0,0 +1,26 @@
+-- | Types used by various components of the I2P library
+module Network.Anonymous.I2P.Types where
+
+-- | Socket types
+data SocketType =
+  -- | Virtual streams are guaranteed to be sent reliably and in order, with
+  -- failure and success notification as soon as it is available. Streams are
+  -- bidirectional communication sockets between two I2P destinations, but their
+  -- opening has to be requested by one of them.
+  VirtualStream |
+
+  -- | While I2P doesn't inherently contain a FROM address, for ease of use
+  -- an additional layer is provided as repliable datagrams - unordered
+  -- and unreliable messages of up to 31744 bytes that include a FROM
+  -- address (leaving up to 1KB for header material).  This FROM address
+  -- is authenticated internally by SAM (making use of the destination's
+  -- signing key to verify the source) and includes replay prevention.
+  DatagramRepliable |
+
+  -- | Squeezing the most out of I2P's bandwidth, SAM allows clients to send
+  -- and receive anonymous datagrams, leaving authentication and reply
+  -- information up to the client themselves.  These datagrams are
+  -- unreliable and unordered, and may be up to 32768 bytes.
+  DatagramAnonymous
+
+  deriving (Eq, Show)
