68e3465b1b42440bfa0e59b17a4811fdbe6f9b50
Leon Mergen <leon@solatis.com>
2015-02-14 16:46:35 +0700

    Makes debug conditional again.

--- src/Network/Anonymous/I2P/Internal/Debug.hs
+++ src/Network/Anonymous/I2P/Internal/Debug.hs
@@ -4,8 +4,13 @@
 -- | Debugging helper functions, for internal use only
 module Network.Anonymous.I2P.Internal.Debug where
 
-import Debug.Trace
+import Debug.Trace (trace)
 
 -- | Alias to Debug.Trace(trace), but disabled in non-debug builds
 log :: String -> a -> a
+
+#ifdef DEBUG
 log = trace
+#else
+log _ ret = ret
+#endif
