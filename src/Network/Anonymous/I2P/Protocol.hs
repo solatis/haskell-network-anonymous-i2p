5599b8da781b54c18ddd4ec9cc254d7f9947c110
Leon Mergen <leon@solatis.com>
2015-02-22 16:44:50 +0700

    Implements session create protocol
    Implements first attempt at the session create part of the protocol.
    Still needs to be tested.

--- src/Network/Anonymous/I2P/Protocol.hs
+++ src/Network/Anonymous/I2P/Protocol.hs
@@ -3,16 +3,22 @@
 -- | Protocol description
 --
 -- Defines functions that handle the advancing of the SAMv3 protocol.
-module Network.Anonymous.I2P.Protocol (NST.connect, version, versionWithConstraint) where
+module Network.Anonymous.I2P.Protocol ( NST.connect
+                                      , version
+                                      , versionWithConstraint
+                                      , session) where
 
 import           Control.Monad.Catch
 import           Control.Monad.IO.Class
 
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as TE
+import qualified Data.Text                             as T
+import qualified Data.Text.Encoding                    as TE
+import qualified Data.UUID                             as Uuid
+import qualified Data.UUID.V4                          as Uuid
 
-import qualified Data.ByteString as BS
 import qualified Data.Attoparsec.ByteString            as Atto
+import qualified Data.ByteString                       as BS
+import qualified Data.ByteString.Char8                 as BS8
 import qualified Network.Simple.TCP                    as NST
 
 import qualified Network.Socket                        as Network
@@ -20,8 +26,9 @@ import qualified Network.Socket.ByteString             as Network
 
 import qualified Network.Attoparsec                    as NA
 
-import qualified Network.Anonymous.I2P.Protocol.Parser as Parser
 import qualified Network.Anonymous.I2P.Internal.Debug  as D
+import qualified Network.Anonymous.I2P.Protocol.Parser as Parser
+import           Network.Anonymous.I2P.Types           (SocketType (..))
 
 -- | Announces ourselves with SAM bridge and negotiates protocol version
 --
@@ -61,6 +68,41 @@ versionWithConstraint (minV, maxV) (s, _) =
     res <- NA.parseOne s (Atto.parse Parser.version)
 
     case res of
-     Parser.VersionResultOk v       -> D.log ("got version: " ++ show v) (return v)
-     Parser.VersionResultNone       -> D.log "no good version found" (fail "No version could be negotiated")
+     Parser.VersionResultOk v       -> D.log ("got version: " ++ show v)      (return v)
+     Parser.VersionResultNone       -> D.log "no good version found"          (fail "No version could be negotiated")
      Parser.VersionResultError msg  -> D.log ("protocol error: " ++ show msg) (fail ("An error occured while negotiating version: " ++ msg))
+
+
+session :: ( MonadIO m
+           , MonadMask m)
+        => SocketType                         -- ^ I2P socket type to create
+        -> (Network.Socket, Network.SockAddr) -- ^ Our connection with SAM bridge
+        -> m (String, String)                 -- ^ Our session id and our private destination key
+session socketType (s, _) =
+  let socketTypeToString :: SocketType -> BS.ByteString
+      socketTypeToString VirtualStream     = "STREAM"
+      socketTypeToString DatagramRepliable = "DATAGRAM"
+      socketTypeToString DatagramAnonymous = "RAW"
+
+      createSessionId :: IO String
+      createSessionId =
+        return . Uuid.toString =<< Uuid.nextRandom
+
+      versionString :: String -> BS.ByteString
+      versionString sessionId =
+        BS.concat [ "SESSION CREATE STYLE=", socketTypeToString socketType, " "
+                  , "ID=\"", BS8.pack sessionId, "\" "
+                  , "DESTINATION=TRANSIENT "
+                  , "SIGNATURE_TYPE=EDDSA_SHA512_ED25519"]
+
+  in do
+    sessionId <- liftIO $ createSessionId
+    liftIO $ Network.sendAll s (versionString sessionId)
+    res <- NA.parseOne s (Atto.parse Parser.session)
+
+    case res of
+     Parser.SessionResultOk d           -> D.log ("got destination: " ++ show d)  (return (sessionId, d))
+     Parser.SessionResultDuplicatedId   -> D.log "duplicated session id"          (fail "Session id already in use")
+     Parser.SessionResultDuplicatedDest -> D.log "duplicated destination"         (fail "Destination already in use")
+     Parser.SessionResultInvalidKey     -> D.log "invalid destination id"         (fail "Destination is not a valid private destination key")
+     Parser.SessionResultError msg      -> D.log ("protocol error: " ++ show msg) (fail ("An error occured while creating session: " ++ msg))
