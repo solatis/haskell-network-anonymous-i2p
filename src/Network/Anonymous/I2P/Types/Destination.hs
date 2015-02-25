c0566c9e1f426056f74a89532bb56b5a13675cba
Leon Mergen <leon@solatis.com>
2015-02-25 10:37:29 +0700

    Improves documentation

--- src/Network/Anonymous/I2P/Types/Destination.hs
+++ src/Network/Anonymous/I2P/Types/Destination.hs
@@ -6,14 +6,11 @@ import qualified Data.ByteString as BS
 -- | I2P destination
 --
 --   This type represents the *encoded*, base64 representation of an I2P
---   destination. According to the I2P documentation at this url:
---
---     https://geti2p.net/en/docs/spec/common-structures#struct_Destination
---
---   the destination is a concatenation of the destination, encryption key
---   and the signing key. However, since these are variable length, it is
---   not practical to actually decode them, since we do not have a use for
---   them anyway.
+--   destination. According to the <https://geti2p.net/en/docs/spec/common-structures#struct_Destination I2P documentation>
+--   the destination is a concatenation of the destination, encryption key and
+--   the signing key. However, since these are variable length, it is not
+--   practical to actually decode them, since we do not have a use for them
+--   anyway.
 --
 --   Furthermore, when testing using the haskell base64 library, apparently
 --   the destination is not valid base64 either.
