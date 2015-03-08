11c3aed05d9fde7fc52d2079d5b5b7d7535452cf
Leon Mergen <leon@solatis.com>
2015-03-08 11:35:32 +0700

    Refactors response parsing
    Now generalizes the sending of output to the socket and the parsing of
    the response in the 'expectResponse' function.

--- src/Network/Anonymous/I2P/Protocol.hs
+++ src/Network/Anonymous/I2P/Protocol.hs
@@ -39,6 +39,30 @@ import qualified Network.Anonymous.I2P.Protocol.Parser.Ast as Ast
 import qualified Network.Anonymous.I2P.Types.Destination   as D
 import qualified Network.Anonymous.I2P.Types.Socket        as S
 
+
+-- | According to the I2P protocol, the first two tokens in a response are always
+--   in a fixed position, and for each step in the protocol, we expect two very
+--   specific keys to be here.
+--
+--   This is a function that sends a buffer to a socket, waits for and parses the
+--   respone, and returns the remaining tokens.
+expectResponse :: ( MonadIO m
+                  , MonadMask m)
+               => Network.Socket
+               -> BS.ByteString
+               -> (BS.ByteString, BS.ByteString)
+               -> m [Ast.Token]
+expectResponse sock output (first, second) = do
+    liftIO $ putStrLn ("Sending output: " ++ show output)
+    liftIO $ Network.sendAll sock output
+    res <- NA.parseOne sock (Atto.parse Parser.line)
+
+    case res of
+     (Ast.Token first' Nothing : Ast.Token second' Nothing : xs) -> if (first == first' && second == second')
+                                                                    then return xs
+                                                                    else E.i2pError (E.mkI2PError E.protocolErrorType)
+     _                                                           -> E.i2pError (E.mkI2PError E.protocolErrorType)
+
 -- | Announces ourselves with SAM bridge and negotiates protocol version
 --
 --   Defaults to protocol version 3.1, which is the only one we support at the
@@ -60,7 +84,7 @@ versionWithConstraint :: ( MonadIO m
                       -> (Network.Socket, Network.SockAddr) -- ^ Our connection with SAM bridge
                       -> m [Integer]                        -- ^ Version agreed upon, stores as a list of integers; for
                                                             --   example, [3,1] means version 3.1
-versionWithConstraint (minV, maxV) (s, _) =
+versionWithConstraint (minV, maxV) (sock, _) =
   let versionToString :: [Integer] -> BS.ByteString
       versionToString vs =
         let textList :: [Integer] -> [T.Text]
@@ -78,19 +102,14 @@ versionWithConstraint (minV, maxV) (s, _) =
       versionParser = (Atto8.decimal `Atto.sepBy` Atto8.char '.')
 
   in do
-    liftIO $ Network.sendAll s helloString
-    res <- NA.parseOne s (Atto.parse Parser.line)
+    res <- expectResponse sock helloString ("HELLO", "REPLY")
+    case (Ast.value                 "RESULT"  res,
+          Ast.valueAs versionParser "VERSION" res) of
 
-    case res of
-     (Ast.Token "HELLO" Nothing : Ast.Token "REPLY" Nothing : xs) ->
-       case (Ast.value                 "RESULT"  xs,
-             Ast.valueAs versionParser "VERSION" xs) of
-
-        -- This is the normal result, and 'VERSION' will contain our (parsed) version
-        (Just ("OK"),        Just v) -> return v
-        (Just ("NOVERSION"), _)      -> E.i2pError (E.mkI2PError E.noVersionErrorType)
-        _                            -> E.i2pError (E.mkI2PError E.protocolErrorType)
-     _                               -> E.i2pError (E.mkI2PError E.protocolErrorType)
+     -- This is the normal result, and 'VERSION' will contain our (parsed) version
+     (Just ("OK"),        Just v) -> return v
+     (Just ("NOVERSION"), _)      -> E.i2pError (E.mkI2PError E.noVersionErrorType)
+     _                            -> E.i2pError (E.mkI2PError E.protocolErrorType)
 
 -- | Creates a new I2P public/private destination pair
 createDestination :: ( MonadIO m
@@ -120,19 +139,13 @@ createDestination signature (sock, _) =
                   , "\n"]
 
   in do
-    liftIO $ putStrLn ("Sending createDestinationString: " ++ show createDestinationString)
-    liftIO $ Network.sendAll sock createDestinationString
-    res <- NA.parseOne sock (Atto.parse Parser.line)
+    res <- expectResponse sock createDestinationString ("DEST", "REPLY")
+    case (Ast.value "PRIV" res, Ast.value "PUB" res)  of
+     (Just priv, Just pub) -> D.log
+                                ("created destination, priv = " ++ show priv ++ ", pub = " ++ show pub)
+                                return (D.PrivateDestination priv, D.PublicDestination pub)
 
-    case res of
-     (Ast.Token "DEST" Nothing : Ast.Token "REPLY" Nothing : xs) ->
-       case (Ast.value "PRIV" xs, Ast.value "PUB" xs)  of
-        (Just priv, Just pub) -> D.log
-                                   ("created destination, priv = " ++ show priv ++ ", pub = " ++ show pub)
-                                   return (D.PrivateDestination priv, D.PublicDestination pub)
-
-        _                     -> E.i2pError (E.mkI2PError E.protocolErrorType)
-     _ -> E.i2pError (E.mkI2PError E.protocolErrorType)
+     _                     -> E.i2pError (E.mkI2PError E.protocolErrorType)
 
 -- | Create a session with default parameters provided.
 createSession :: ( MonadIO m
@@ -168,35 +181,28 @@ createSessionWith Nothing destination socketType pair = do
     ("created session id: " ++ show uuid)
     createSessionWith (Just (Uuid.toString uuid)) destination socketType pair
 
-createSessionWith (Just sessionId) destination socketType (s, _) =
+createSessionWith (Just sessionId) destination socketType (sock, _) =
   let socketTypeToString :: S.SocketType -> BS.ByteString
       socketTypeToString S.VirtualStream     = "STREAM"
       socketTypeToString S.DatagramRepliable = "DATAGRAM"
       socketTypeToString S.DatagramAnonymous = "RAW"
 
-      versionString :: String -> BS.ByteString
-      versionString sid =
+      sessionString :: String -> BS.ByteString
+      sessionString sid =
         BS.concat [ "SESSION CREATE STYLE=", socketTypeToString socketType, " "
                   , "ID=", BS8.pack sid, " "
                   , "DESTINATION=", D.asByteString destination
                   , "\n"]
 
   in do
-    liftIO $ putStrLn ("Sending version string: " ++ show (versionString sessionId))
-    liftIO $ Network.sendAll s (versionString sessionId)
-    res <- NA.parseOne s (Atto.parse Parser.line)
-
-    case res of
-     (Ast.Token "SESSION" Nothing : Ast.Token "STATUS" Nothing : xs) ->
-       case Ast.value "RESULT" xs of
-
-        -- This is the normal result, and 'VERSION' will contain our (parsed) version
-        Just ("OK")              -> return sessionId
-        Just ("DUPLICATED_ID")   -> E.i2pError (E.mkI2PError E.duplicatedSessionIdErrorType)
-        Just ("DUPLICATED_DEST") -> E.i2pError (E.mkI2PError E.duplicatedDestinationErrorType)
-        Just ("INVALID_KEY")     -> E.i2pError (E.mkI2PError E.invalidKeyErrorType)
-        _                        -> E.i2pError (E.mkI2PError E.protocolErrorType)
-     _                           -> E.i2pError (E.mkI2PError E.protocolErrorType)
+    res <- expectResponse sock (sessionString sessionId) ("SESSION", "STATUS")
+    case Ast.value "RESULT" res of
+     -- This is the normal result, and 'VERSION' will contain our (parsed) version
+     Just ("OK")              -> return sessionId
+     Just ("DUPLICATED_ID")   -> E.i2pError (E.mkI2PError E.duplicatedSessionIdErrorType)
+     Just ("DUPLICATED_DEST") -> E.i2pError (E.mkI2PError E.duplicatedDestinationErrorType)
+     Just ("INVALID_KEY")     -> E.i2pError (E.mkI2PError E.invalidKeyErrorType)
+     _                        -> E.i2pError (E.mkI2PError E.protocolErrorType)
 
 -- | For VirtualStream sockets, accepts one new connection
 acceptStream :: ( MonadIO m
@@ -224,21 +230,14 @@ acceptStream sessionId (sock, _) =
           return (D.PublicDestination buf)
 
   in do
-    liftIO $ putStrLn ("Sending acceptString: " ++ show (acceptString sessionId))
-    liftIO $ Network.sendAll sock (acceptString sessionId)
-    res <- NA.parseOne sock (Atto.parse Parser.line)
-
-    case res of
-     (Ast.Token "STREAM" Nothing : Ast.Token "STATUS" Nothing : xs) ->
-       case Ast.value "RESULT" xs of
-
-        -- This is the normal result, and 'VERSION' will contain our (parsed) version
-        Just ("OK")         -> do
-          dst <- readDestination sock
-          return (sock, dst)
-        Just ("INVALID_ID") -> E.i2pError (E.mkI2PError E.invalidIdErrorType)
-        _                   -> E.i2pError (E.mkI2PError E.protocolErrorType)
-     _                      -> E.i2pError (E.mkI2PError E.protocolErrorType)
+    res <- expectResponse sock (acceptString sessionId) ("STREAM", "STATUS")
+    case Ast.value "RESULT" res of
+      -- This is the normal result, and 'VERSION' will contain our (parsed) version
+      Just ("OK")         -> do
+        dst <- readDestination sock
+        return (sock, dst)
+      Just ("INVALID_ID") -> E.i2pError (E.mkI2PError E.invalidIdErrorType)
+      _                   -> E.i2pError (E.mkI2PError E.protocolErrorType)
 
 -- | For VirtualStream sockets, establishes connection with a remote
 connectStream :: ( MonadIO m
@@ -259,19 +258,12 @@ connectStream sessionId destination (sock, _) =
                   , "\n"]
 
   in do
-    liftIO $ putStrLn ("Sending connectString: " ++ show (connectString sessionId))
-    liftIO $ Network.sendAll sock (connectString sessionId)
-    res <- NA.parseOne sock (Atto.parse Parser.line)
-
-    case res of
-     (Ast.Token "STREAM" Nothing : Ast.Token "STATUS" Nothing : xs) ->
-       case Ast.value "RESULT" xs of
-
-        -- This is the normal result, and 'VERSION' will contain our (parsed) version
-        Just ("OK")              -> return ()
-        Just ("INVALID_ID")      -> E.i2pError (E.mkI2PError E.invalidIdErrorType)
-        Just ("INVALID_KEY")     -> E.i2pError (E.mkI2PError E.invalidKeyErrorType)
-        Just ("TIMEOUT")         -> E.i2pError (E.mkI2PError E.timeoutErrorType)
-        Just ("CANT_REACH_PEER") -> E.i2pError (E.mkI2PError E.unreachableErrorType)
-        _                        -> E.i2pError (E.mkI2PError E.protocolErrorType)
-     _                           -> E.i2pError (E.mkI2PError E.protocolErrorType)
+    res <- expectResponse sock (connectString sessionId) ("STREAM", "STATUS")
+    case Ast.value "RESULT" res of
+     -- This is the normal result, and 'VERSION' will contain our (parsed) version
+     Just ("OK")              -> return ()
+     Just ("INVALID_ID")      -> E.i2pError (E.mkI2PError E.invalidIdErrorType)
+     Just ("INVALID_KEY")     -> E.i2pError (E.mkI2PError E.invalidKeyErrorType)
+     Just ("TIMEOUT")         -> E.i2pError (E.mkI2PError E.timeoutErrorType)
+     Just ("CANT_REACH_PEER") -> E.i2pError (E.mkI2PError E.unreachableErrorType)
+     _                        -> E.i2pError (E.mkI2PError E.protocolErrorType)
